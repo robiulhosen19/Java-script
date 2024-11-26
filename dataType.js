722da31a41632c13e231e2902108aa06b0a23d54
Robiul Hosen <robiul.pu.cse.19@gmail.com>
2024-11-26 23:54:13 +0600

    DataType

---
+++ dataType.js
@@ -0,0 +1,54 @@
+//DATA TYPE IN JAVASCRIPT 
+// PRIMITIVES DATA TYPE 
+// COMPLEX DATA TYPE 
+// OBJECT DATA TYPE
+// ARRAY DATA TYPE
+// FUNCTION DATA TYPE
+ //BOOLEAN 
+ // NUMBER
+ // STRING
+ // NULL
+ // undefined
+ // COMPLEX DATA TYPE
+
+
+ // 1. Boolean
+    let val = true;
+    console.log(val); // true
+    console.log(typeof val); // boolean
+//2. number 
+    let num = 45; 
+    console.log(typeof num); //Number
+//3. string 
+
+let text = "hello world";
+console.log(text);
+console.log(typeof text);
+
+//4.NULL
+
+let nullVal = null;
+console.log(nullVal);
+console.log(typeof nullVal);
+
+//5. undefined
+let undef = undefined;
+console.log(undef);
+console.log(typeof undef);
+
+//symbol 
+let sym = Symbol();
+console.log(sym);
+console.log(typeof sym);
+
+//object 
+let obj = {
+    name: "John",
+    age: 25,
+    address: "New York"
+};
+console.log(obj);
+console.log(typeof obj);
+
+let arr = ["sojib", "robiul"];
+console.log(typeof arr);
